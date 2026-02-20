e78459097a90b943075b520d8c90d771f9d0f874
Juliodvp29 <juliodevep29@gmail.com>
2026-02-20 09:51:39 -0500

    feat: add Google Generative AI dependency and environment configuration files with API key.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  geminiApiKey: 'AIzaSyA7s-h_OvE7-EdJOZfrY6l3rZbd0xxyUxU',
   supabase: {
     url: 'https://msjkjymlvjaliaztlbls.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zamtqeW1sdmphbGlhenRsYmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyMjE3NjYsImV4cCI6MjA4Njc5Nzc2Nn0.kKSLKSENOdLC_jqvTDr1jpW-MG1HzRMlk322SAsHL0U',
