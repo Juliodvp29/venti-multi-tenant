db38e98e817c4c8faf138547312f5cd581bf619c
julionewdev29-sketch <julionewdev29@gmail.com>
2026-09-02 21:41:55 -0500

    chore: add ESLint configuration and dependencies for Angular project
    - Added ESLint configuration file to enforce coding standards and best practices.
    - Included necessary ESLint plugins for Angular and RxJS.
    - Updated package.json to include ESLint and related dependencies.

---
+++ eslint.config.js
@@ -0,0 +1,63 @@
+const eslint = require('@eslint/js');
+const angular = require('angular-eslint');
+const rxjs = require('eslint-plugin-rxjs-x').default;
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  {
+    ignores: [
+      '**/dist/**',
+      '**/node_modules/**',
+      '**/.angular/**',
+      '**/coverage/**',
+      'supabase/functions/**',
+      'src/environments/environment.ts',
+      'src/environments/environment.prod.ts',
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    plugins: {
+      rxjs,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
+      'no-empty': 'warn',
+      'no-useless-assignment': 'warn',
+      'prefer-const': 'warn',
+      '@angular-eslint/no-empty-lifecycle-method': 'warn',
+      '@angular-eslint/no-output-native': 'warn',
+      '@angular-eslint/no-output-on-prefix': 'warn',
+      '@angular-eslint/prefer-inject': 'warn',
+      '@angular-eslint/prefer-on-push-component-change-detection': 'warn',
+      'rxjs/no-ignored-subscribe': 'warn',
+      'rxjs/no-exposed-subjects': 'warn',
+      'rxjs/no-unsafe-takeuntil': 'warn',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: Object.fromEntries(
+      Object.keys(angular.templatePlugin.rules).map((ruleName) => [
+        `@angular-eslint/template/${ruleName}`,
+        'warn',
+      ]),
+    ),
+  },
+);
